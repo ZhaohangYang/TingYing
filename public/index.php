197c895ce96a85ee6009c64eba837ff2d9a279c5
yangzhaohang <yangzhaohang@comsenz-service.com>
2020-10-13 08:47:55 +0800

    引入http——foundation

--- public/index.php
+++ public/index.php
@@ -1,18 +1,24 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(-1);
+require_once __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../vendor/autoload.php';
-
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
+$request = Request::create('/index.php?name=Fabien');
 
-$app = require_once __DIR__ . '/../core/container.php';
+// $response->setContent('Hello world!');
+// $response->setStatusCode(200);
+// $response->headers->set('Content-Type', 'text/html');
 
-$response = $app->get('hummingbird')
-    ->handle(
-        Symfony\Component\HttpFoundation\Request::createFromGlobals()
-    );
+// configure the HTTP cache headers
+// $response->setMaxAge(10);
+
+
+// $request = Request::createFromGlobals();
+
+$name = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
 
 $response->send();
