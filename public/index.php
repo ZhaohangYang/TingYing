b7f663d3f86b8df78880f26e7091d6b1b2fafae1
yangzhaohang <yangzhaohang@comsenz-service.com>
2020-12-04 10:05:55 +0800

    大小写bug

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 define('START_TIME', microtime(true));
 define('BASE_PATH', str_replace('\\', '/', realpath(dirname(__FILE__) . '/../')));
-$container = require_once __DIR__ . '/../framework/container/container.php';
+$container = require_once __DIR__ . '/../framework/container/Container.php';
 
 $response = $container->get('hummingbird')
     ->handle(
